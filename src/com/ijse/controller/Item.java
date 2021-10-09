c3bd078d96d367c55e24ce94a74c09505191def9
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-10-09 11:20:55 +0530

    create Item stage

---
+++ src/com/ijse/controller/Item.java
@@ -0,0 +1,13 @@
+package com.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Item {
+    public TextField txtCode;
+    public TextField txtDescription;
+    public TextField txtPrice;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+}
