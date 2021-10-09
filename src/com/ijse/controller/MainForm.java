a83fb549d8bddf4d75018df0d0e8273fc32f8937
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-10-09 10:19:57 +0530

    adding childPane to MainForm stage

--- src/com/ijse/controller/MainForm.java
+++ src/com/ijse/controller/MainForm.java
@@ -1,8 +1,11 @@
 package com.ijse.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class MainForm {
+    public AnchorPane childPane;
+
     public void btnCustomerPaneOnAction(ActionEvent actionEvent) {
     }
 
