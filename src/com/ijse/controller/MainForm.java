74cd536c47ff611785392507fe6b97285a549dd5
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-10-09 10:29:21 +0530

    load Customer stage when clicking the customer button in MainForm stage

--- src/com/ijse/controller/MainForm.java
+++ src/com/ijse/controller/MainForm.java
@@ -1,12 +1,18 @@
 package com.ijse.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class MainForm {
     public AnchorPane childPane;
 
-    public void btnCustomerPaneOnAction(ActionEvent actionEvent) {
+    public void btnCustomerPaneOnAction(ActionEvent actionEvent) throws IOException {
+        childPane.getChildren().clear();
+        AnchorPane pane = FXMLLoader.load(this.getClass().getResource("../view/Customer.fxml"));
+        childPane.getChildren().setAll(pane.getChildren());
     }
 
     public void btnItemPaneOnAction(ActionEvent actionEvent) {
