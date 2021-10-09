6313ade62cff7f84215ea8f45cb0077b889390bb
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-10-09 11:39:38 +0530

    finished save item issue close #2

--- src/com/ijse/controller/Item.java
+++ src/com/ijse/controller/Item.java
@@ -1,13 +1,19 @@
 package com.ijse.controller;
 
+import com.ijse.model.ItemModel;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
 
 public class Item {
     public TextField txtCode;
     public TextField txtDescription;
     public TextField txtPrice;
+    private ItemDBController itemDBController;
 
-    public void btnSaveOnAction(ActionEvent actionEvent) {
+    public void btnSaveOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        ItemModel itemModel = new ItemModel(txtCode.getText(), txtDescription.getText(), txtPrice.getText());
+        itemDBController.addItem(itemModel);
     }
 }
