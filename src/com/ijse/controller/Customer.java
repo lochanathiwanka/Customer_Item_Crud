1017768b5bc246a20dae4c19ec3e81d06d51c92b
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-10-09 10:24:52 +0530

    create Customer stage

---
+++ src/com/ijse/controller/Customer.java
@@ -0,0 +1,13 @@
+package com.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Customer {
+    public TextField txtCid;
+    public TextField txtName;
+    public TextField txtAddress;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+}
